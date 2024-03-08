6147ad83b873cf280f027c660f17b2294b1d73fe
mirvision <florangorr@gmail.com>
2024-03-08 18:36:28 +0500

    1470. Shuffle the Array

---
+++ shuffleTheArray.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func shuffle(nums []int, n int) []int {
+	result := []int{}
+	for i := 0; i < n; i++ {
+		result = append(result, nums[i], nums[i+n])
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(shuffle([]int{2, 5, 1, 3, 4, 7}, 3))
+}
