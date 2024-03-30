bb71a559df71a0cb0f7ce361e259133a2d5a3f7a
mirvision <florangorr@gmail.com>
2024-03-30 22:55:59 +0500

    125. Valid Palindrome

---
+++ twoSumII.go
@@ -0,0 +1,10 @@
+package main
+
+
+func twoSum(numbers []int, target int) []int {
+    var result []int
+	l, r := 0, 0
+	for l < r {
+		
+	}
+}
---
+++ validePalidrom.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+
+	s = strings.ToLower(s)
+	l, r := 0, len(s)-1
+	for l < r {
+		if !isAlphanumeric(s[l]) {
+			l++
+			continue
+
+		} else if !isAlphanumeric(s[r]) {
+			r--
+			continue
+		}
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+
+	}
+	return true
+}
+func isAlphanumeric(char byte) bool {
+	return (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z')
+}
+
+func main() {
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(isPalindrome("race a car"))
+	fmt.Println(isPalindrome(" "))
+
+}
