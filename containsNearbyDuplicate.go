af031b7d57c5b31eceec9b0826c7e3573b4096bf
mirvision <florangorr@gmail.com>
2024-04-19 14:39:06 +0500

    219. Contains Duplicate II

--- containsNearbyDuplicate.go
+++ containsNearbyDuplicate.go
@@ -3,30 +3,21 @@ package main
 import "fmt"
 
 func containsNearbyDuplicate(nums []int, k int) bool {
+	m := make(map[int]int)
 	l := 0
-	r := 0
-
-	for i := 0; i < k; i++ {
-		l += nums[i]
-	}
-	r = l
-
-	for i := k; i < len(nums); i++ {
-		fmt.Println(l, r)
-		r = r - nums[i-k] + nums[i]
-		l = max(l, r)
+	for r, num := range nums {
+		if r-l > k {
+			delete(m, nums[l])
+			l++
+		}
+		if _, found := m[num]; found {
+			return true
+		}
+		m[num] = r
 
 	}
 	return false
 }
-
-func max(x int, y int) int {
-	if x < y {
-		return y
-	}
-	return x
+func main() {
+	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1}, 3))
 }
-
-// func main() {
-// 	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1}, 3))
-// }
