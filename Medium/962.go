c985bc17af578787256bd39a2187a390a64632e8
mirvision <florangorr@gmail.com>
2024-05-01 20:59:30 +0500

    962. Maximum Width Ramp

---
+++ Medium/962.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func maxWidthRamp(nums []int) int {
+	var stack []int
+	var res int
+
+	for i := 0; i < len(nums); i++ {
+		if len(stack) == 0 || nums[i] < nums[stack[len(stack)-1]] {
+			stack = append(stack, i)
+		}
+	}
+	for i := len(nums) - 1; i >= 0; i-- {
+		for len(stack) != 0 && nums[i] >= nums[stack[len(stack)-1]] {
+			width := i - stack[len(stack)-1]
+			if width > res {
+				res = width
+			}
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+
+	}
+
+	if res == 0 {
+		return -1
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(maxWidthRamp([]int{6, 0, 8, 2, 1, 5})) //4
+}
